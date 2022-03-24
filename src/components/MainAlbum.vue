7b290b2154d33c183d61d5bd423dadc38a21cfef
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-24 16:42:20 +0100

    Bonus Page Loader component created

--- src/components/MainAlbum.vue
+++ src/components/MainAlbum.vue
@@ -1,12 +1,15 @@
 <template>
   <main class="bg-secondary p-5">
-      <div class="row row-cols-lg-6 g-3">
+      <div v-if="pageLoaded == false" class="server-delay">
+          <page-loader />
+      </div>
+      <div v-else class="row row-cols-lg-6 g-3">
         <card-album v-for="(album, index) in arrAlbum" :key="index" 
         :albumPoster="album.poster"
         :albmTitle="album.title"
         :albumAuthor="album.author"
         :albumYear="album.year"
-        :albumGenre="album.genre"
+        :albumGenre="`genre-${album.genre}`"
         />
       </div>
   </main>
@@ -14,25 +17,39 @@
 
 <script>
 import CardAlbum from './CardAlbum.vue'
+import PageLoader from './PageLoader.vue'
 import axios from 'axios'
 export default {
     name: 'MainAlbum',
     components: {
-        CardAlbum
+        CardAlbum,
+        PageLoader,
     },
     data () {
         return {
             arrAlbum: [],
+            networkError: false,
+            responseDelay: 3000,
+            pageLoaded: false,
+
         }
     },
     created () {
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-            .then((ele) => {
-                ele.data.response.forEach(ele => {
-                    this.arrAlbum.push(ele);
+        setTimeout (() => {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((ele) => {
+                    this.pageLoaded = true;
+                    ele.data.response.forEach(ele => {
+                        this.arrAlbum.push(ele);
+                })
+            
+                    
+            }) .catch(error => {
+                    // handle error
+                    console.log(error);
+                    this.networkError = true;
                 });
-                
-        })
+        }, this.responseDelay);
     },
 };
 </script>
@@ -41,5 +58,11 @@ export default {
     .row {
         max-width: 1000px;
         margin: 0 auto;
+    }
+    .server-delay {
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        height:80vh;
     }
 </style>
---
+++ src/components/PageLoader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="loader">
+            <div l a></div>
+            <div l b></div>
+            <div l c></div>
+            <div l d></div>
+            <div l e></div>
+            <div l f></div>
+            <div l g></div>
+            <div l h></div>
+           
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'PageLoader',
+}
+</script>
+
+<style scoped lang="scss">
+    #loader{
+        height:80px;
+        width:80px;
+        position:relative;
+        }[l]{
+        position:absolute;
+        width:10px;
+        height:10px;
+        border-radius:50%;
+        animation:loader 1.6s linear infinite;
+        }
+        [a]{left:35px;}
+        [b]{right:10px;top:10px;animation-delay:.2s}
+        [c]{top:35px;right:0;animation-delay:.4s}
+        [d]{right:10px;bottom:10px;animation-delay:.6s}
+        [e]{left:35px;bottom:0;animation-delay:.8s}
+        [f]{left:10px;bottom:10px;animation-delay:1s}
+        [g]{top:35px;left:0;animation-delay:1.2s}
+        [h]{left:10px;top:10px;animation-delay:1.4s}
+        @keyframes loader{
+        0%{
+            background:none;
+        }
+        100%{
+            background:white;
+        }
+    }
+</style>
