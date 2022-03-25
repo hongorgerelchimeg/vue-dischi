cecfab71befbc60911408976de129441efab6bc5
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-25 15:38:00 +0100

    new SearchBar component created

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent=""> 
+    <select v-model="strSearch" @change="emitSearch" class="custom-select">
+        <option v-for="(options, index) in arrOptions" :key="index" :value="options">{{options}}</option>
+    </select>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data () {
+        return {
+            strSearch: '',
+            arrOptions: [],
+        }
+    },
+    methods: {
+        emitSearch () {
+            this.$emit('search', this.strSearch);
+            console.log(this.strSearch);
+        }
+    },
+    props: {
+       arrAlbumProp: Array,
+    },
+    created () {
+        this.arrOptions = this.arrAlbumProp;
+    }
+}
+</script>
+
+<style>
+
+</style>
